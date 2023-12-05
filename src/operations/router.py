bb88927950d8677a193715e83bddbc29e30ab925
Garik <garfild_2003@mail.ru>
2023-12-06 00:23:10 +0300

    Added a simple endpoint to get a list of operations with the type specified by user

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,3 +1,23 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.database import get_async_session
+from src.operations.models import Operation
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/operations",
+    tags=["Operation"]
+)
+
+
+@router.get("/")
+async def get_specific_operations(operation_type: str, session: AsyncSession = Depends(get_async_session)):
+    """
+    Функция возвращает список операций с конкретным типом, заданным пользователем
+    """
+    q = select(Operation).filter(Operation.type == operation_type)
+    result = await session.execute(q)
+    operations = result.mappings().all()
+    return operations
+
+
