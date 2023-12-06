cd4f4961b6bc46b7a4a8d9f72fc46e9fa2929d78
Garik <garfild_2003@mail.ru>
2023-12-06 17:10:58 +0300

    Added a function for writing data to the Operation table

--- src/operations/router.py
+++ src/operations/router.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, Depends
-from sqlalchemy import select
+from sqlalchemy import select, insert
 from sqlalchemy.ext.asyncio import AsyncSession
 from src.database import get_async_session
 from src.operations.models import Operation
+from src.operations.schemas import OperationCreate
 
 router = APIRouter(
     prefix="/operations",
@@ -20,4 +21,14 @@ async def get_specific_operations(operation_type: str, session: AsyncSession = D
     operations = result.mappings().all()
     return operations
 
+
+@router.post("/")
+async def add_specific_operations(new_operation: OperationCreate, session: AsyncSession = Depends(get_async_session)):
+    """
+    Функция для записи данных в таблицу операций
+    """
+    stmt = insert(Operation).values(**new_operation.dict())
+    await session.execute(stmt)
+    await session.commit()
+    return {"status", "success"}
 
---
+++ src/operations/schemas.py
@@ -0,0 +1,10 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class OperationCreate(BaseModel):
+    quantity: str
+    figi: str
+    instrument_type: str
+    date: datetime
+    type: str
